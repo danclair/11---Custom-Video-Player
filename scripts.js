34ad0b9ad2a4d1ce69b4519f9e2cf99921b3e8cb
Scott Geryol <sgeryol@gmail.com>
2020-06-22 20:13:54 -0500

    finailzied video player

--- scripts.js
+++ scripts.js
@@ -20,8 +20,37 @@ function updateButton() {
   // console.log("Update the button");
 }
 
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis - `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = [e.offsetX / progress.offsetWidth] * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e);
+}
+
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) => range.addEventListener("mousemove", handleRangeUpdate));
+
+let mousedown = false;
+progressBar.addEventListener("click", scrub);
+progressBar.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
