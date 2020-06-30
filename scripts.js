ca1dfcd06f20d507964bdfddbc95dd675ab276b9
Ethan Rader <rader.ethan@gmail.com>
2020-06-29 20:23:38 -0500

    rudimentary language controls for play pause and skip

--- scripts.js
+++ scripts.js
@@ -70,6 +70,47 @@ function toggleInvert() {
   }
 }
 
+// start voice recognition section
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener("result", (e) => {
+  const transcript = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+
+  p.textContent = transcript;
+  if (e.results[0].isFinal) {
+    p = document.createElement("p");
+    words.appendChild(p);
+  }
+  if (transcript.includes("play video") && e.results[0].isFinal && video.paused) {
+    togglePlay();
+  }
+  if (transcript.includes("pause video") && e.results[0].isFinal && !video.paused) {
+    togglePlay();
+  }
+  if (transcript.includes("skip forward") && e.results[0].isFinal) {
+    video.currentTime += parseFloat(10);
+  }
+  if (transcript.includes("skip backward") && e.results[0].isFinal) {
+    video.currentTime -= parseFloat(10);
+  }
+  console.log(transcript);
+});
+
+recognition.addEventListener("end", recognition.start);
+
+recognition.start();
+
 // Hook up event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
